f51ee73b4101d2109602135d3adb12d9aea55a7b
teffalban <teff30a@gmail.com>
2025-06-17 19:53:40 -0500

    cambio nombre (proyecto por consorcio)

--- src/Components/NewProject/NewProject.tsx
+++ src/Components/NewProject/NewProject.tsx
@@ -108,9 +108,9 @@ const NewProject: React.FC = () => {
           onChange={handleModeChange}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         >
-          <option value="create">Crear Proyecto</option>
-          <option value="update">Actualizar Proyecto</option>
-          <option value="deactivate">Desactivar Proyecto</option>
+          <option value="create">Crear  Consorcio</option>
+          <option value="update">Actualizar Consorcio</option>
+          <option value="deactivate">Desactivar Consorcio</option>
         </select>
       </div>
 
